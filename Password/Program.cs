cc5ba42e1f41e9f84350493c23e97afe95043021
AleksandrGHub <bulanov.shura@gmail.com>
2024-06-05 21:08:12 +0300

    Добавьте файлы проекта.

---
+++ Password/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityAttempts = 3;
+            string password = "12345";
+            string userInput;
+
+            while (quantityAttempts > 0)
+            {
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Пароль подобран!");
+                }
+                else
+                {
+                    quantityAttempts--;
+                    Console.WriteLine("Введите пароль еще раз!");
+                }
+            }
+        }
+    }
+}
